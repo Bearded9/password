0e0e7edd1a9d6d883a48bf468c666b08b6610115
Alexandr Bryukhanov <alexbruyhanov@yandex.ru>
2023-04-21 00:12:01 +0700

    Исправил замечания из комментариев, сейчас работаю над тем, чтобы рандомились каждый раз новые символы, пока залил то что сделал

--- password.py
+++ password.py
@@ -1,4 +1,5 @@
 import random
+import utils
 
 num_list = "0123456789"
 small_letter_list = "abcdefghijklmnopqrstuvwxyz"
@@ -6,41 +7,36 @@ big_letter_list = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 special_symbols_list = """!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""
 
 print("Выберите нужные пункты, после пароль сгенерируется")
-chars = ""
-chars2 = ""
-num = input("Цифры 0-9, да/нет ")
+character_set = ""
+generated_password = ""
+num = input("Цифры 0-9, да/нет ") 
 small_letter = input("Маленькие буквы, a-z да/нет ")
 big_letter = input("Большие буквы, A-Z да/нет ")
 special_symbols = input("Спец. символы, да/нет ")
-password_length = int(input("Введите длинну пароля от 4 до 64: "))
-while password_length < 4 or password_length > 64:
-    print("Вы ввели число за пределами диапазона, повторите попытку")
-    password_length = int(input("Введите длинну пароля от 4 до 64: "))
-generate_password = int(input("Сколько вариантов пароля сгенерировать(введите от 1 до 10) "))
-while generate_password < 1 or generate_password > 10:
-    print("Вы ввели число за пределами диапазона, повторите попытку")
-    generate_password = int(input("Сколько вариантов пароля сгенерировать(введите от 1 до 10) "))
+password_length = utils.number_check(4, 64)
+number_of_passwords = utils.number_check_lenght(1, 10)
 
-if num == "да":
-    chars += random.choice(num_list)
-    chars2 += num_list
-if small_letter == "да":
-    chars += random.choice(small_letter_list)
-    chars2 += small_letter_list
-if big_letter == "да":
-    chars += random.choice(big_letter_list)
-    chars2 += big_letter_list
-if special_symbols == "да":
-    chars += random.choice(special_symbols_list)
-    chars2 += special_symbols_list
+if num == "да" or num == "ДА" or num == "Да":
+    character_set += random.choice(num_list)
+    generated_password += num_list
+if small_letter == "да" or small_letter == "ДА" or small_letter == "Да":
+    character_set += random.choice(small_letter_list)
+    generated_password += small_letter_list
+if big_letter == "да" or big_letter == "ДА" or big_letter == "Да":
+    character_set += random.choice(big_letter_list)
+    generated_password += big_letter_list
+if special_symbols == "да" or special_symbols == "ДА" or special_symbols == "Да":
+    character_set += random.choice(special_symbols_list)
+    generated_password += special_symbols_list
     
-if generate_password >= 1 and generate_password <= 10:
-    for i in range(generate_password):
-        password = chars
-        for n in range(password_length - len(chars)):
-            password += random.choice(chars2)
-        mix_password = password.split()
-        for i, word in enumerate(map(list, mix_password)):
-            random.shuffle(word)
-            mix_password[i] = ''.join(word)
-        print(*mix_password)
+if number_of_passwords >= 1 and number_of_passwords <= 10:
+    for i in range(number_of_passwords):
+        password = character_set
+        for n in range(password_length - len(character_set)):
+            password += random.choice(generated_password)
+            password = list(password)
+            random.shuffle(password)
+            password_mix = ""
+        for n in password:
+            password_mix += n
+        print(password_mix)
